efa747c9a96642010f615cb7a6793c7b4125a484
ping gong <gpzlx1@mail.ustc.edu.cn>
2025-08-02 12:55:25 +0000

    update run_pred.py

--- tasks/run_pred.py
+++ tasks/run_pred.py
@@ -42,17 +42,98 @@ def seed_everything(seed):
     torch.cuda.manual_seed_all(seed)
 
 
-def pred_loop_func(args, rank, task_queue, dataset_manager):
-    seed_everything(args.seed)
+def generate_dataloader_and_kwargs(args, dataset_name,
+                                   dataset_manager, tokenizer, eos_token_id, apply_chat_template, generate_kwargs):
+    raw_data = dataset_manager.get_data(dataset_name)
+    _, dataset_maxlen, dataset_category = dataset_manager.get_dataset_info(
+        dataset_name)
 
-    args.method = args.method.lower()
+    process_fn = partial(
+        dataset_manager.process_raw_data,
+        tokenizer=tokenizer,
+        apply_chat_template=apply_chat_template,
+        task=dataset_name,
+        max_length=args.model_maxlen,
+        truncate_from_middle=True,
+    )
 
-    model_meta, model_config, tokenizer, _, _ = load_config_and_tokenizer(
-        args, args.model_name_or_path)
-    model = load_model(model_meta, model_config, args.model_name_or_path)
-    model.generation_config.pad_token_id = tokenizer.pad_token_id
+    remove_columns = []
+    for key in raw_data[0]:
+        if key not in [
+                "length",
+                "all_classes",
+                "answers",
+                "depth_percent",
+                "difficulty",
+                "domain",
+                "sub_domain",
+                "answer",
+                "canonical_solution",
+                "test",
+                "entry_point",
+                "answerKey",
+        ]:
+            remove_columns.append(key)
+    encoded_data = raw_data.map(
+        process_fn,
+        batched=True,
+        num_proc=4,
+        batch_size=10,
+        with_indices=True,
+        remove_columns=remove_columns,
+    )
+    dataloader = torch.utils.data.DataLoader(
+        encoded_data,
+        batch_size=args.batch_size,
+        collate_fn=DefaultDataCollator(tokenizer=tokenizer),
+        pin_memory=False,
+    )
 
-    device_ids = [args.pp_num * rank + i for i in range(args.pp_num)]
+    generate_kwargs["max_new_tokens"] = dataset_maxlen
+    if dataset_name in [
+            "2wikimqa",
+            "hotpotqa",
+            "musique",
+            "multifieldqa_en",
+            "qasper",
+            "narrativeqa",
+            "samsum",
+    ]:
+        generate_kwargs["eos_token_id"] = eos_token_id
+        if dataset_category is not None and "QA" in dataset_category:
+            generate_kwargs["eos_token_id"].append(
+                tokenizer.encode("\n", add_special_tokens=False)[-1])
+    else:
+        generate_kwargs.pop("eos_token_id", None)
+
+    return dataloader, generate_kwargs
+
+
+def run_one_pred_and_write(args, x, model, tokenizer, dataset_name, dataset_manager, generate_kwargs, warm_up):
+    out_info = x.copy()
+
+    x["input_ids"] = x["input_ids"].to(model.device)
+    x["attention_mask"] = x["attention_mask"].to(model.device)
+
+    preserve_domins = ["input_ids", "attention_mask"]
+
+    for key in out_info:
+        num_obj = len(out_info[key])
+        if key not in preserve_domins:
+            x.pop(key)
+
+    for key in preserve_domins:
+        out_info.pop(key)
+
+    outputs = comm_generate(x, generate_kwargs, model, tokenizer)
+
+    if not warm_up:
+        for i in range(num_obj):
+            dataset_manager.write_one_result_v3(
+                args.output_dir, outputs[i], i, out_info, dataset_name)
+
+
+def dispatch_model_to_devices(model, device_ids):
     if len(device_ids) > 1:
         max_memory = {}
         for id in device_ids:
@@ -61,8 +142,6 @@ def pred_loop_func(args, rank, task_queue, dataset_manager):
         device_map = infer_auto_device_map(model,
                                            no_split_module_classes=[
                                                "CustomLlamaDecoderLayer",
-                                               "CustomGlmDecoderLayer",
-                                               "CustomGLMBlock",
                                                "CustomQwen2DecoderLayer"
                                            ],
                                            **map_kwargs)
@@ -74,8 +153,69 @@ def pred_loop_func(args, rank, task_queue, dataset_manager):
 
     for id in device_ids:
         print(
-            f"Worker {rank} use GPU[{id}] mem: {torch.cuda.max_memory_allocated(id) / 1024 ** 3:.2f} GB"
+            f"Worker use GPU[{id}] mem: {torch.cuda.max_memory_allocated(id) / 1024 ** 3:.2f} GB"
         )
+
+    return model
+
+
+def single_pref(args, dataset_manager, tasks):
+    # load model and tokenizer
+    args.method = args.method.lower()
+    model_meta, model_config, tokenizer, original_generate_kwargs, apply_chat_template = load_config_and_tokenizer(
+        args, args.model_name_or_path)
+    model = load_model(model_meta, model_config, args.model_name_or_path)
+    model.generation_config.pad_token_id = tokenizer.pad_token_id
+
+    device_ids = [i for i in range(args.pp_num)]
+    model = dispatch_model_to_devices(model, device_ids)
+
+    # set generate_kwargs
+    if hasattr(model_config, "eos_token_id"):
+        eos_token_id = model_config.eos_token_id
+    else:
+        eos_token_id = tokenizer.eos_token_id
+    if isinstance(eos_token_id, int):
+        eos_token_id = [eos_token_id]
+
+    warm_up = True
+
+    for dataset_name in tasks:
+        dataloader, generate_kwargs = generate_dataloader_and_kwargs(
+            args, dataset_name, dataset_manager, tokenizer, eos_token_id, apply_chat_template,
+            original_generate_kwargs)
+
+        if warm_up:
+            for _, x in enumerate(tqdm(dataloader, desc=f"Warm up")):
+                if x["input_ids"].size(-1) < args.min_seq_len:
+                    continue
+
+                run_one_pred_and_write(args,
+                                       x, model, tokenizer,  dataset_name, dataset_manager, generate_kwargs, warm_up)
+
+                warm_up = False
+                break
+
+        for _, x in enumerate(
+                tqdm(dataloader, desc=f"Send tasks for {dataset_name}")):
+            if x["input_ids"].size(-1) < args.min_seq_len:
+                continue
+            run_one_pred_and_write(args,
+                                   x, model, tokenizer, dataset_name, dataset_manager, generate_kwargs, warm_up)
+
+
+def pred_loop_func(args, rank, task_queue, dataset_manager):
+    seed_everything(args.seed)
+
+    args.method = args.method.lower()
+
+    model_meta, model_config, tokenizer, _, _ = load_config_and_tokenizer(
+        args, args.model_name_or_path)
+    model = load_model(model_meta, model_config, args.model_name_or_path)
+    model.generation_config.pad_token_id = tokenizer.pad_token_id
+
+    device_ids = [args.pp_num * rank + i for i in range(args.pp_num)]
+    model = dispatch_model_to_devices(model, device_ids)
 
     try:
         signal.alarm(timeout)  # start timer
@@ -84,29 +224,9 @@ def pred_loop_func(args, rank, task_queue, dataset_manager):
             if data is None:
                 break
             warm_up, dataset_name, x, generate_kwargs = data
-            indices = x.pop("index").tolist()
 
-            out_info = x.copy()
-
-            x["input_ids"] = x["input_ids"].to(model.device)
-            x["attention_mask"] = x["attention_mask"].to(model.device)
-
-            preserve_domins = ["input_ids", "attention_mask"]
-
-            for key in out_info:
-                num_obj = len(out_info[key])
-                if key not in preserve_domins:
-                    x.pop(key)
-
-            for key in preserve_domins:
-                out_info.pop(key)
-
-            outputs = comm_generate(x, generate_kwargs, model, tokenizer)
-
-            if not warm_up:
-                for i in range(num_obj):
-                    dataset_manager.write_one_result_v3(
-                        args.output_dir, outputs[i], i, out_info, dataset_name)
+            run_one_pred_and_write(args,
+                                   x, model, tokenizer, dataset_name, dataset_manager, generate_kwargs, warm_up)
 
             # reset timer
             signal.alarm(timeout)
@@ -124,7 +244,7 @@ def pred_loop_func(args, rank, task_queue, dataset_manager):
                     f"Total prefill times: {model.model.layers[l].self_attn.req_num}"
                 )
                 model.model.layers[l].self_attn.timer.check_all_recoder()
-    except:
+    except AttributeError:
         print(f"Breakdown not implemented for {args.method}!")
 
 
@@ -163,20 +283,22 @@ if __name__ == "__main__":
         args.dataset_name, args.dataset_path, args.e)
     print("datasets: ", tasks)
 
+    if args.mp_num <= 1:
+        # single process prediction
+        single_pref(args, dataset_manager, tasks)
+        exit()
+
     # load model and tokenizer
     args.method = args.method.lower()
-    model_meta, model_config, tokenizer, generate_kwargs, apply_chat_template = load_config_and_tokenizer(
+    model_meta, model_config, tokenizer, generate_kwarg, apply_chat_template = load_config_and_tokenizer(
         args, args.model_name_or_path)
 
-    # set generate_kwargs
     if hasattr(model_config, "eos_token_id"):
         eos_token_id = model_config.eos_token_id
     else:
         eos_token_id = tokenizer.eos_token_id
     if isinstance(eos_token_id, int):
         eos_token_id = [eos_token_id]
-
-    exit()
 
     # start processes
     task_queue = mp.Queue(maxsize=args.mp_num)
@@ -192,68 +314,9 @@ if __name__ == "__main__":
     warm_up = True
 
     for dataset_name in tasks:
-        raw_data = dataset_manager.get_data(dataset_name)
-        _, dataset_maxlen, dataset_category = dataset_manager.get_dataset_info(
-            dataset_name)
-
-        process_fn = partial(
-            dataset_manager.process_raw_data,
-            tokenizer=tokenizer,
-            apply_chat_template=apply_chat_template,
-            task=dataset_name,
-            max_length=args.model_maxlen,
-            truncate_from_middle=True,
-        )
-
-        remove_columns = []
-        for key in raw_data[0]:
-            if key not in [
-                    "length",
-                    "all_classes",
-                    "answers",
-                    "depth_percent",
-                    "difficulty",
-                    "domain",
-                    "sub_domain",
-                    "answer",
-                    "canonical_solution",
-                    "test",
-                    "entry_point",
-                    "answerKey",
-            ]:
-                remove_columns.append(key)
-        encoded_data = raw_data.map(
-            process_fn,
-            batched=True,
-            num_proc=4,
-            batch_size=10,
-            with_indices=True,
-            remove_columns=remove_columns,
-        )
-
-        dataloader = torch.utils.data.DataLoader(
-            encoded_data,
-            batch_size=args.batch_size,
-            collate_fn=DefaultDataCollator(tokenizer=tokenizer),
-            pin_memory=False,
-        )
-
-        generate_kwargs["max_new_tokens"] = dataset_maxlen
-        if dataset_name in [
-                "2wikimqa",
-                "hotpotqa",
-                "musique",
-                "multifieldqa_en",
-                "qasper",
-                "narrativeqa",
-                "samsum",
-        ]:
-            generate_kwargs["eos_token_id"] = eos_token_id
-            if dataset_category is not None and "QA" in dataset_category:
-                generate_kwargs["eos_token_id"].append(
-                    tokenizer.encode("\n", add_special_tokens=False)[-1])
-        else:
-            generate_kwargs.pop("eos_token_id", None)
+        dataloader, generate_kwargs = generate_dataloader_and_kwargs(
+            args, dataset_name, dataset_manager, tokenizer, eos_token_id, apply_chat_template,
+            generate_kwarg)
 
         if warm_up:
             for i, x in enumerate(tqdm(dataloader, desc=f"Warm up")):
